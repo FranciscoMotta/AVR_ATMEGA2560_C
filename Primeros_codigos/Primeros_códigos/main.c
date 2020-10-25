2f4dda55db4caf327578030daa9f0dfdc32c4ede
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-10-25 03:20:17 -0500

    first commit

---
+++ Primeros_codigos/Primeros_códigos/main.c
@@ -0,0 +1,24 @@
+/*
+ * Primeros_códigos.c
+ *
+ * Created: 25/10/2020 03:07:12
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		int z; /* Creamos una variable del tipo caracter sin signo */
+		DDRB = 0XFF ; /* Ponemos a todo el puerto B como salida */
+		for (z = 0 ; z <= 255 ; z++) {
+			PORTB = z;
+		}
+	}
+	return 0;
+}
+
