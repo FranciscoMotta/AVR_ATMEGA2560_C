63e1eee02e182b3b24c0228b0e5674a4320c6479
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-10-26 02:53:21 -0500

    Añadimos un iterador de estados

---
+++ codeSeis/codeSeis/main.c
@@ -0,0 +1,30 @@
+/*
+ * codeSeis.c
+ *
+ * Created: 26/10/2020 02:50:00
+ * Author : HP
+ */ 
+
+#define F_CPU 16000000UL	
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    /*Replace with your application code*/
+	
+	DDRB = 0XFF;
+	
+	// HACEMOS UN ITERADOR DE ESTADOS SIN EL NEGADOR ~
+	
+    while (1) 
+    {
+		PORTB = 0XAA;
+		_delay_ms(1000);
+		PORTB = 0XBB;
+		_delay_ms(1000);
+    }
+	return 0;
+}
+
