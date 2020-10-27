b175f50103394366ced882b756e7840b5295ef14
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-10-27 17:21:13 -0500

    añadimos comentarios al codigo del LCD

--- restoDeCode/LCD_PRIMER_MODELO_SIMPLE/main.c
+++ restoDeCode/LCD_PRIMER_MODELO_SIMPLE/main.c
@@ -13,17 +13,17 @@
 #define ENABLE 1
 #define RS 0
 
-void modoCaracter (void) {
-	PORTC |= (1 << RS);
+void modoCaracter (void) { //PONEMOS AL LCD EN MODO CARACTER PARA MOSTRAR EL MENSAJE
+	PORTC |= (1 << RS); //PONEMOS AL PIN 0 COMO 1 
 	_delay_ms(3);
 }
 
-void modoComando (void){
-	PORTC &= ~ (1 << RS);
+void modoComando (void){ //PONEMOS AL LCD EN MODO COMANDO PARA CONFIGURAR SUS CARACTERÍSTICAS
+	PORTC &= ~ (1 << RS); //PONEMOS AL PIN 0 COMO 0
 	_delay_ms(3);
 }
 
-void tiempoEnable (void){
+void tiempoEnable (void){ //HACEMOS UNA FUNCIÓN PARA PODER CONTROLAR EL ENABLE
 	PORTC |= (1 << ENABLE);
 	_delay_ms(2);
 	PORTC &= ~ (1 << ENABLE);
@@ -32,9 +32,9 @@ void tiempoEnable (void){
 
 int main(void)
 {
-	unsigned char dataConf[4] = {0x01, 0x38, 0x06, 0x0c};
-	unsigned char textmat[15] = {'H','O','L','A',' ','A',' ','T','O','D','O','S'};
-	int counterLocal = 0;
+	unsigned char dataConf[4] = {0x01, 0x38, 0x06, 0x0c}; //CREAMOS UN ARREGLO DE DATOS DE CONFIGURACIÓN
+	unsigned char textmat[15] = {'H','O','L','A',' ','A',' ','T','O','D','O','S'}; //CREAMOS UN ARREGLO PARA EL MENSAJE A MOSTRAR
+	int counterLocal = 0; //UN CONTADOR PARA SACAR LOS DATOS DE CADA ARREGLO
 	DDRB = 0XFF;
 	DDRC = 0xFF;
     /* Replace with your application code */
