96fa887eea2a5f417ccd3a0d3a8d9175d5a66b6f
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-10-30 17:54:31 -0500

    LCD with registers finish!

--- Manejo de pines/Manejo de pines/main.c
+++ Manejo de pines/Manejo de pines/main.c
@@ -11,6 +11,8 @@
 
 #define pinSerial 1 
 #define clockLCD 0
+#define ENABLE 3
+#define RS 2
 
 void tiempoEnable (void){ //HACEMOS UNA FUNCIÓN PARA PODER CONTROLAR EL ENABLE
 	PORTC |= (1 << clockLCD);
@@ -19,16 +21,41 @@ void tiempoEnable (void){ //HACEMOS UNA FUNCIÓN PARA PODER CONTROLAR EL ENABLE
 	_delay_ms(1);
 }
 
+void Modo_Comando(void){
+	PORTC &= ~(1 << RS);
+	_delay_ms(1);
+}
+
+void Modo_Caracter(void){
+	PORTC |= (1 << RS);
+	_delay_ms(1);
+}
+
+void Tiempo_De_Enable (void) {
+	PORTC |= (1 << ENABLE);
+	_delay_ms(3);
+	PORTC &= ~(1 << ENABLE);
+	_delay_ms(3);
+}
+
 void Desplazador_De_Datos( char dato ){
 		unsigned char byteParalelo = dato;
 		unsigned char registroDesplazamiento = 0;
 		unsigned char contadorDeDesplazamiento = 0;
+		
 		registroDesplazamiento = byteParalelo;
 		for (contadorDeDesplazamiento = 0 ; contadorDeDesplazamiento < 8 ; contadorDeDesplazamiento++){
 			if(registroDesplazamiento & 0x01){
-				PORTC |= (1<<pinSerial);
+				PORTC |= (1<< pinSerial);
+				//PORTBbits.RB0  = 0 ; NO EXISTE!!!!
+				//PUERTO AVR POR LO GENERAL TIENE 8 BITS -> 1 BYTE 
+				//REGISTRO ES EL PORTX -> A,B,C,D,E,F,G,H
+				// << # >> # DESPLAZAR UN BIT
+				// PORTB = (1 << 5) = (0B00100000) PORTCbits.RC5 
+				//suma = suma or (1 << pinSerial) 
 				} else {
 				PORTC &= ~(1 << pinSerial);
+				// PORTC = PORTC & ~(1 << pinSerial)
 			}
 			tiempoEnable();
 			_delay_ms(1);
@@ -36,17 +63,26 @@ void Desplazador_De_Datos( char dato ){
 		}	
 }
 
-int main(void)
-{
-	DDRC = 0x0F;
-	
-	 
+int main(void){
+	DDRC = 0X0F;
+	//Areglos para el manejo del LCD
+		
+	unsigned char datosConf[4] = {0x01, 0x38,0x0C, 0X06};
+	unsigned char mensajeLcd[15] = {'H','O','L','A', ' ', 'L','C','D',' ','C','O','N',' ','F','F'};
+		
+	int contador_Local = 0;
     /* Replace with your application code */
     while (1){
-		Desplazador_De_Datos(0xAA);
-		_delay_ms(1000);
-		Desplazador_De_Datos(0x55);
-		_delay_ms(1000);
+		Modo_Comando();
+		for(contador_Local = 0; contador_Local < 4 ; contador_Local ++){
+			Desplazador_De_Datos(datosConf[contador_Local]);
+			Tiempo_De_Enable();	
+		}
+		Modo_Caracter();
+		for (contador_Local = 0 ; contador_Local < 15 ; contador_Local ++){
+			Desplazador_De_Datos(mensajeLcd[contador_Local]);
+			Tiempo_De_Enable();
+		}
 		while(1);	
     }
 	return 0;
